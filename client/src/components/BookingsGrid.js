4963bfce60035737d555e254a1e752fbf7c37ef6
mirkiy <mirizachar@gmail.com>
2022-01-19 14:05:29 +0000

    created bookings grid

--- client/src/components/BookingsGrid.js
+++ client/src/components/BookingsGrid.js
@@ -1,12 +1,16 @@
 import React from "react";
 import BookingsCard from "./BookingsCard";
 
-const BookingsGrid = () =>{
+const BookingsGrid = ({guestBookings, deleteBooking}) => {
+
+    const bookingList = guestBookings.map((booking) => {
+        return <BookingsCard guestBookings={guestBookings} deleteBooking={deleteBooking} key={booking._id}/>
+    });
 
     return (
         <>
         <h2>BookingsGrid</h2>
-         <BookingsCard/> 
+         {bookingList}
         </>
     )
 }
