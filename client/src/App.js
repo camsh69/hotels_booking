523a18af582e644d0c4e38d92f21b3be8e348260
mirkiy <mirizachar@gmail.com>
2022-01-19 12:03:43 +0000

    BookingsService added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,18 @@ import React, {useEffect, useState} from "react";
 import './App.css';
 import BookingsGrid from './components/BookingsGrid';
 import BookingsForm from "./components/BookingsForm";
+import {getBookings, deleteBooking, postBooking} from "./BookingsService"
 
 function App() {
+  
+  const [guestBookings, setGuestBookings] = useState([]);
+
+  useEffect(() => {
+    getBookings().then((allBookings) => {
+      setGuestBookings(allBookings);
+    })
+  }, []);
+  
   return (
     <>
       <h1>  hello world !</h1>
